017490f41cb5e07d714c2a284e278500c58629e6
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2014-03-11 17:10:05 -0300

    removing unnecessary files and folders

--- app.js
+++ app.js
@@ -3,20 +3,16 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-var http = require('http');
-var path = require('path');
-
-var app = express();
-
-var fortune = require('./routes/fortune');
+var express = require('express'),
+    http    = require('http'),
+    path    = require('path'),
+    fortune = require('./lib/fortune'),
+    app     = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-// app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
@@ -30,10 +26,26 @@ if ('development' == app.get('env')) {
 }
 
 // Index
-app.get('/', routes.index);
+app.get('/', function(req, res){
+  res.render('index', { title: 'Fortune API using Node.js and Express.js' });
+});
 
 // Fortunes
-app.get('/fortune?.:format?', fortune.show);
+app.get('/fortune?.:format?', function(req, res){
+
+  var long  = req.query.long,
+      dirty = req.query.dirty,
+      text  = fortune(long, dirty).text();
+
+  if (req.params.format === 'json') {
+    res.set('Content-Type', 'application/json');
+    res.json({ text: text });
+  } else {
+    res.set('Content-Type', 'text/plain');
+    res.send(text);
+  };
+
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- spec/app_spec.js
+++ spec/app_spec.js
@@ -1,5 +1,23 @@
 var request = require('request');
 
+describe("index", function(){
+
+  it("responds successfully", function(done){
+    request("http://localhost:3000", function(error, response, body){
+      expect(response.statusCode).toEqual(200);
+      done();
+    });
+  });
+
+  it("has the correct body", function(done){
+    request("http://localhost:3000", function(error, response, body){
+      expect(response.body).toContain("Node.js Fortune API");
+      done();
+    });
+  });
+
+});
+
 describe("fortune", function(){
 
   describe("as JSON", function(){
